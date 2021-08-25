6229a44bcee0f1350dffa21e978bb81f90b43252
tugamars <mars.silva.13@gmail.com>
2021-08-25 19:43:15 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,6 +2,184 @@ Config = {
 	
 	Command = "sling",
 	Weapons =  {
+		--- Assault Rifle Mk2
+		["961495388"]={
+			objectName="w_ar_assaultrifle",
+			allowone=true,
+			attachaments={
+				[0x8610343F] = { bone = "WAPClip", objectName = "w_ar_assaultrifle_mag1" },
+				[0xD12ACA6F] = { bone = "WAPClip", objectName = "w_ar_assaultrifle_mag2" },
+				[0xEF2C78C1] = { bone = "WAPClip", objectName = "w_ar_assaultrifle_mag1" },
+				[0xFB70D853] = { bone = "WAPClip", objectName = "w_ar_assaultrifle_mag1" },
+				[0xA7DD1E58] = { bone = "WAPClip", objectName = "w_ar_assaultrifle_mag1" },
+				[0x63E0A098] = { bone = "WAPClip", objectName = "w_ar_assaultrifle_mag1" },
+				[0x7BC4CDDC] = { bone = "WAPFlshLasr", objectName = "w_at_ar_flsh" },
+				[0x49B2945] = { bone = "WAPScop", objectName = "w_at_scope_macro_2" },
+				[0xC66B6542] = { bone = "WAPScop", objectName = "w_at_scope_medium" },
+				[0xA73D4664] = { bone = "WAPSupp", objectName = "w_at_ar_supp_02" },
+				[0x420FD713] = { },
+				[0x9D65907A] = { },
+				[0xB99402D4] = { },
+				[0xDE11CBCF] = { },
+				[0xEC9068CC] = { },
+				[0x2E7957A] = { },
+				[0x347EF8AC] = { },
+				[0x4DB62ABE] = { },
+				[0x43A49D26] = { },
+				[0x5646C26A] = { },
+				[0x911B24AF] = { },
+				[0x37E5444B] = { },
+				[0x538B7B97] = { },
+				[0x25789F72] = { },
+				[0xC5495F2D] = { },
+				[0xCF8B73B1] = { },
+				[0xA9BB2811] = { },
+				[0xFC674D54] = { },
+				[0x7C7FCD9B] = { },
+				[0xA5C38392] = { },
+				[0xB9B15DB0] = { }
+			}
+		},
+		--- Assault Rifle
+		["-1074790547"]={
+			objectName="w_ar_assaultrifle",
+			allowone=true,
+			attachaments={
+				[0xBE5EEA16] = { bone = "WAPClip", objectName = "w_ar_assaultrifle_mag1" },
+				[0xB1214F9B] = { bone = "WAPClip", objectName = "w_ar_assaultrifle_mag2" },
+				[0xDBF0A53D] = { bone = "WAPClip", objectName = "w_ar_assaultrifle_mag2" },
+				[0x7BC4CDDC] = { bone = "WAPFlshLasr", objectName = "w_at_ar_flsh" },
+				[0x9D2FBF29] = { bone = "WAPScop", objectName = "w_at_scope_macro_2" },
+				[0xA73D4664] = { bone = "WAPSupp", objectName = "w_at_ar_supp_02" },
+				[0xC164F53] = { },
+				[0x4EAD7533] = { }
+			}
+		},
+		--- Combat MG Mk2
+		["3686625920"]={
+			objectName="w_mg_combatmg",
+			allowone=true,
+			attachaments={
+				[0x492B257C] = { bone = "WAPClip", objectName = "w_mg_combatmg_mag1" },
+				[0x17DF42E9] = { bone = "WAPClip", objectName = "w_mg_combatmg_mag2" },
+				[0xF6649745] = { bone = "WAPClip", objectName = "w_mg_combatmg_mag1" },
+				[0xC326BDBA] = { bone = "WAPClip", objectName = "w_mg_combatmg_mag1" },
+				[0x29882423] = { bone = "WAPClip", objectName = "w_mg_combatmg_mag1" },
+				[0x57EF1CC8] = { bone = "WAPClip", objectName = "w_mg_combatmg_mag1" },
+				[0x420FD713] = { },
+				[0x3F3C8181] = { bone = "WAPScop", objectName = "w_at_scope_small_2" },
+				[0xC66B6542] = { bone = "WAPScop", objectName = "w_at_scope_medium" },
+				[0xB99402D4] = { },
+				[0xC867A07B] = { },
+				[0xDE11CBCF] = { },
+				[0xEC9068CC] = { },
+				[0x2E7957A] = { },
+				[0x347EF8AC] = { },
+				[0x4DB62ABE] = { },
+				[0xC34EF234] = { },
+				[0xB5E2575B] = { },
+				[0x4A768CB5] = { },
+				[0xCCE06BBD] = { },
+				[0xBE94CF26] = { },
+				[0x7609BE11] = { },
+				[0x48AF6351] = { },
+				[0x9186750A] = { },
+				[0x84555AA8] = { },
+				[0x1B4C088B] = { },
+				[0xE046DFC] = { },
+				[0x28B536E] = { },
+				[0xD703C94D] = { }
+			}
+		},
+		--- MG
+		["-1660422300"]={
+			objectName="w_mg_mg",
+			allowone=true,
+			attachaments={
+				[0xF434EF84] = { bone = "WAPClip", objectName = "w_mg_mg_mag1" },
+				[0x82158B47] = { bone = "WAPClip", objectName = "w_mg_mg_mag2" },
+				[0x3C00AFED] = { bone = "WAPScop", objectName = "w_at_scope_small_2" },
+				[0xD6DABABE] = { }
+			}
+		},
+		--- Combat MG
+		["2144741730"]={
+			objectName="w_mg_combatmg",
+			allowone=true,
+			attachaments={
+				[0xE1FFB34A] = { bone = "WAPClip", objectName = "w_mg_combatmg_mag1" },
+				[0xD6C59CD6] = { bone = "WAPClip", objectName = "w_mg_combatmg_mag2" },
+				[0xA0D89C42] = { bone = "WAPScop", objectName = "w_at_scope_medium" },
+				[0xC164F53] = { },
+				[0x92FECCDD] = { }
+			}
+		},
+		--- Assault SMG
+		["-270015777"]={
+			objectName="w_sb_assaultsmg",
+			allowone=true,
+			attachaments={
+				[0x8D1307B0] = { bone = "WAPClip", objectName = "w_sb_assaultsmg_mag1" },
+				[0xBB46E417] = { bone = "WAPClip", objectName = "w_sb_assaultsmg_mag2" },
+				[0x7BC4CDDC] = { bone = "WAPFlshLasr", objectName = "w_at_ar_flsh" },
+				[0x9D2FBF29] = { bone = "WAPScop", objectName = "w_at_scope_macro" },
+				[0xA73D4664] = { bone = "WAPSupp", objectName = "w_at_ar_supp_02" },
+				[0x278C78AF] = { }
+			}
+		},
+		--- MicroSMG
+		["324215364"]={
+			objectName="w_sb_microsmg",
+			allowone=true,
+			attachaments={
+				[0xCB48AEF0] = { bone = "WAPClip", objectName = "w_sb_microsmg_mag1" },
+				[0x10E6BA2B] = { bone = "WAPClip", objectName = "w_sb_microsmg_mag2" },
+				[0x359B7AAE] = { bone = "WAPFlshLasr", objectName = "w_at_pi_flsh" },
+				[0x9D2FBF29] = { bone = "WAPScop", objectName = "w_at_scope_macro" },
+				[0xA73D4664] = { bone = "WAPSupp", objectName = "w_at_ar_supp_02" },
+				[0x487AAE09] = { }
+			}
+		},
+		--- SMG MK2
+		["2024373456"]={
+			objectName="w_sb_smg",
+			allowone=true,
+			attachaments={
+				[0x4C24806E] = { bone = "WAPClip", objectName = "w_sb_smg_mag1" },
+				[0xB9835B2E] = { bone = "WAPClip", objectName = "w_sb_smg_mag2" },
+				[0x7FEA36EC] = { bone = "WAPClip", objectName = "w_sb_smg_mag1" },
+				[0x7FEA36EC] = { bone = "WAPClip", objectName = "w_sb_smg_mag1" },
+				[0xD99222E5] = { bone = "WAPClip", objectName = "w_sb_smg_mag1" },
+				[0x3A1BD6FA] = { bone = "WAPClip", objectName = "w_sb_smg_mag1" },
+				[0xB5A715F] = { bone = "WAPClip", objectName = "w_sb_smg_mag1" },
+				[0x7BC4CDDC] = { bone = "WAPFlshLasr", objectName = "w_at_ar_flsh" },
+				[0x9FDB5652] = { },
+				[0xE502AB6B] = { bone = "WAPScop", objectName = "w_at_scope_macro_2" },
+				[0x3DECC7DA] = { bone = "WAPScop", objectName = "w_at_scope_small_2" },
+				[0xC304849A] = { bone = "WAPSupp", objectName = "w_at_pi_supp" },
+				[0xB99402D4] = { },
+				[0xC867A07B] = { },
+				[0xDE11CBCF] = { },
+				[0xEC9068CC] = { },
+				[0x2E7957A] = { },
+				[0x347EF8AC] = { },
+				[0x4DB62ABE] = { },
+				[0xD9103EE1] = { },
+				[0xA564D78B] = { },
+				[0xC4979067] = { },
+				[0x3815A945] = { },
+				[0x4B4B4FB0] = { },
+				[0xEC729200] = { },
+				[0x48F64B22] = { },
+				[0x35992468] = { },
+				[0x24B782A5] = { },
+				[0xA2E67F01] = { },
+				[0x2218FD68] = { },
+				[0x45C5C3C5] = { },
+				[0x399D558F] = { }
+			}
+		},
+		--- SMG
 		["736523883"]={
 			objectName="w_sb_smg",
 			allowone=true,
@@ -15,6 +193,49 @@ Config = {
 				[0x27872C90] = { }
 			}
 		},
+		--- Advanced Rifle
+		["-1357824103"]={
+			objectName="w_ar_advancedrifle",
+			allowone=true,
+			attachaments={
+				[0xFA8FA10F] = { bone = "WAPClip", objectName = "w_ar_advancedrifle_mag1" },
+				[0x8EC1C979] = { bone = "WAPClip", objectName = "w_ar_advancedrifle_mag2" },
+				[0x7BC4CDDC] = { bone = "WAPFlshLasr", objectName = "w_at_ar_flsh" },
+				[0xAA2C45B4] = { bone = "WAPScop", objectName = "w_at_scope_small" },
+				[0x837445AA] = { bone = "WAPSupp", objectName = "w_at_ar_supp" },
+				[0x377CD377] = { }
+			}
+		},
+		--- Special Carabine
+		["-1063057011"]={
+			objectName="w_ar_specialcarbine",
+			allowone=true,
+			attachaments={
+				[0xC6C7E581] = { bone = "WAPClip", objectName = "w_ar_specialcarbine_mag1" },
+				[0x7C8BD10E] = { bone = "WAPClip", objectName = "w_ar_specialcarbine_mag2" },
+				[0x6B59AEAA] = { bone = "WAPClip", objectName = "w_ar_specialcarbine_mag2" },
+				[0x7BC4CDDC] = { bone = "WAPFlshLasr", objectName = "w_at_ar_flsh" },
+				[0xA0D89C42] = { bone = "WAPScop", objectName = "w_at_scope_medium" },
+				[0xA73D4664] = { bone = "WAPSupp", objectName = "w_at_ar_supp" },
+				[0xC164F53] = { },
+				[0x730154F2] = { }
+			}
+		},
+		--- Bullpup rifle
+		["2132975508"]={
+			objectName="w_ar_bullpuprifle",
+			allowone=true,
+			attachaments={
+				[0xC5A12F80] = { bone = "WAPClip", objectName = "w_ar_bullpuprifle_mag1" },
+				[0xB3688B0F] = { bone = "WAPClip", objectName = "w_ar_bullpuprifle_mag2" },
+				[0x7BC4CDDC] = { bone = "WAPFlshLasr", objectName = "w_at_ar_flsh" },
+				[0xAA2C45B4] = { bone = "WAPScop", objectName = "w_at_scope_small" },
+				[0x837445AA] = { bone = "WAPSupp", objectName = "w_at_ar_supp" },
+				[0xC164F53] = { },
+				[0xA857BC78] = { }
+			}
+		},
+		--- Carbine Rifle
 		["-2084633992"]={
 			objectName="w_ar_carbinerifle",
 			allowone=true,
@@ -29,6 +250,46 @@ Config = {
 				[0xD89B9658] = { }
 			}
 		},
+		--- Carbine Rifle Mk2
+		["4208062921"]={
+			objectName="w_ar_carbinerifle",
+			allowone=true,
+			attachaments={
+				[0x4C7A391E] = { bone = "WAPClip", objectName = "w_ar_carbinerifle_mag1" },
+				[0x5DD5DBD5] = { bone = "WAPClip", objectName = "w_ar_carbinerifle_mag2" },
+				[0x1757F566] = { bone = "WAPClip", objectName = "w_ar_carbinerifle_mag1" },
+				[0x3D25C2A7] = { bone = "WAPClip", objectName = "w_ar_carbinerifle_mag1" },
+				[0x255D5D57] = { bone = "WAPClip", objectName = "w_ar_carbinerifle_mag1" },
+				[0x44032F11] = { bone = "WAPClip", objectName = "w_ar_carbinerifle_mag1" },
+				[0x7BC4CDDC] = { bone = "WAPFlshLasr", objectName = "w_at_ar_flsh" },
+				[0x49B2945] = { bone = "WAPScop", objectName = "w_at_scope_macro_2" },
+				[0xC66B6542] = { bone = "WAPScop", objectName = "w_at_scope_medium" },
+				[0x837445AA] = { bone = "WAPSupp", objectName = "w_at_ar_supp" },
+				[0x9D65907A] = { bone = "WAPGrip", objectName = "w_at_ar_afgrip" },
+				[0x420FD713] = { },
+				[0xB99402D4] = { },
+				[0xC867A07B] = { },
+				[0xDE11CBCF] = { },
+				[0xEC9068CC] = { },
+				[0x2E7957A] = { },
+				[0x347EF8AC] = { },
+				[0x4DB62ABE] = { },
+				[0x833637FF] = { },
+				[0x8B3C480B] = { },
+				[0x4BDD6F16] = { },
+				[0x406A7908] = { },
+				[0x2F3856A4] = { },
+				[0xE50C424D] = { },
+				[0xD37D1F2F] = { },
+				[0x86268483] = { },
+				[0xF420E076] = { },
+				[0xAAE14DF8] = { },
+				[0x9893A95D] = { },
+				[0x6B13CD3E] = { },
+				[0xDA55CD3F] = { }
+			}
+		},
+		--- Sniper Rifle
 		["100416529"]={
 			objectName="w_sr_sniperrifle",
 			allowone=false,
@@ -40,6 +301,7 @@ Config = {
 				[0x4032B5E7] = { }
 			}
 		},
+		--- Heavy Sniper
 		["205991906"]={
 			objectName="w_sr_heavysniper",
 			allowone=false,
@@ -49,6 +311,7 @@ Config = {
 				[0xBC54DA77] = { bone = "WAPScop", objectName = "w_at_scope_max" }
 			}
 		},
+		--- Heavy Sniper Mk2
 		["177293209"]={
 			objectName="w_sr_heavysniper",
 			allowone=false,
@@ -81,6 +344,7 @@ Config = {
 				[0x6C32D2EB] = { }
 			}
 		},
+		--- Marksman Rifle
 		["-952879014"]={
 			objectName="w_sr_marksmanrifle",
 			allowone=true,
@@ -94,6 +358,7 @@ Config = {
 				[0x161E9241] = { }
 			}
 		},
+		--- Pump shotgun
 		["487013001"]={
 			objectName="w_sg_pumpshotgun",
 			allowone=true,
@@ -103,6 +368,7 @@ Config = {
 				[0xA2D79DDB] = { }
 			}
 		},
+		--- Pump shotgun mk2
 		["1432025498"]={
 			objectName="w_sg_pumpshotgun",
 			allowone=true,
@@ -132,6 +398,7 @@ Config = {
 				[0x46411A1D] = {}
 			}
 		},
+		--- Sawnoff shotgun
 		["2017895192"]={
 			objectName="w_sg_sawnoff",
 			allowone=true,
@@ -139,6 +406,7 @@ Config = {
 				[0x85A64DF9] = { }
 			}
 		},
+		--- Marksman rifle mk2
 		["1785463520"]={
 			objectName="w_sr_marksmanrifle",
 			allowone=true,
@@ -177,7 +445,6 @@ Config = {
 				[0xB7A316DA] = { },
 			}
 		}
-	},
-	
+	}
 	
 }
